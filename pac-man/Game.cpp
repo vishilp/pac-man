76934396a9a6c5757ba835f58233d0272c850285
vishilp <vishilthe3rd@gmail.com>
2024-11-14 20:32:03 -0800

    Add project files.

---
+++ pac-man/Game.cpp
@@ -0,0 +1,20 @@
+#include "Game.h"
+
+Game::Game() {}
+Game::~Game() {}
+
+void Game::init(const char* title, int xpos, int ypos, int width, int height)
+{
+
+}
+
+void Game::handleEvents() {}
+
+void Game::update() {}
+
+void Game::render() {}
+
+void Game::clean() {}
+
+bool Game::running() { return isRunning; }
+
---
+++ pac-man/main.cpp
@@ -0,0 +1,25 @@
+#include <SDL.h>
+#undef main
+
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(){
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+	
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderDrawPoint(renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+
+	SDL_RenderPresent(renderer);
+	SDL_Delay(10000);
+
+	return 0;
+};

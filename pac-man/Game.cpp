ae94a28f9cc2a8da0e1d4455a1840e45d3d5c357
vishilp <vishilthe3rd@gmail.com>
2024-11-14 23:51:47 -0800

    added some assets, working on outputting a board

--- pac-man/Game.cpp
+++ pac-man/Game.cpp
@@ -5,16 +5,54 @@ Game::~Game() {}
 
 void Game::init(const char* title, int xpos, int ypos, int width, int height)
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		window = SDL_CreateWindow(title, xpos, ypos, width, height, 0);
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer)
+		{
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		}
+		isRunning = true;
+		if (IMG_Init(IMG_INIT_PNG) != 0) {
+			SDL_Surface* tempSurface = IMG_Load("assets/board.png");
+			SDL_Texture* spriteSheet = SDL_CreateTextureFromSurface(renderer, tempSurface);
+			SDL_FreeSurface(tempSurface);
+			SDL_Texture* board = IMG_LoadTexture(renderer, "assets/board.png");
+		}
+	}
+	else
+		isRunning = false;
+}
+
+void Game::handleEvents() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		default:
+			break;
+	}
+}
+
+void Game::update() {
 
 }
 
-void Game::handleEvents() {}
+void Game::render() {
+	SDL_RenderClear(renderer);
+	//add stuff for rendering
 
-void Game::update() {}
+	SDL_RenderPresent(renderer);
+}
 
-void Game::render() {}
-
-void Game::clean() {}
+void Game::clean() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
 
 bool Game::running() { return isRunning; }
 
--- pac-man/main.cpp
+++ pac-man/main.cpp
@@ -1,25 +1,34 @@
 #include <SDL.h>
 #undef main
 
+#include "Game.h"
+
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+Game* game = nullptr;
+
 int main(){
-	SDL_Window* window = nullptr;
-	SDL_Renderer* renderer = nullptr;
+	const int FPS = 60;
+	const int FrameDelay = 1000 / FPS;
+	Uint32 FrameStart;
+	int FrameTime;
 
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
-	
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
+	game = new Game();
+	game->init("Pac-Man", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderDrawPoint(renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+	while (game->running())
+	{
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
 
-	SDL_RenderPresent(renderer);
-	SDL_Delay(10000);
+	game->clean();
+	delete game;
+
+	//board and sprites scaled to 150%
 
 	return 0;
 };

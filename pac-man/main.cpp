5719c0347c6cd96fa3e5bcc4aefc884b05b0e79f
vishilp <vishilthe3rd@gmail.com>
2024-11-16 18:34:47 -0800

    set up the fps limit  to 60

--- pac-man/main.cpp
+++ pac-man/main.cpp
@@ -11,7 +11,7 @@ Game* game = nullptr;
 
 int main(){
 	const int FPS = 60;
-	const int FrameDelay = 1000 / FPS;
+	const int FrameDelay = 1000 / FPS; //time between frames in ms
 	Uint32 FrameStart;
 	int FrameTime;
 
@@ -20,9 +20,18 @@ int main(){
 
 	while (game->running())
 	{
+		FrameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		FrameTime = SDL_GetTicks() - FrameStart;
+
+		if (FrameDelay > FrameTime)
+		{
+			SDL_Delay(FrameDelay - FrameTime);
+		}
 	}
 
 	game->clean();

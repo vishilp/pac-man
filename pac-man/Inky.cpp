ac403c8fb97a073124e66db57f09787c21e905f4
vishilp <vishilthe3rd@gmail.com>
2024-11-29 11:55:23 -0800

    updated inky class, still need to work on his vector calculations

--- pac-man/Inky.cpp
+++ pac-man/Inky.cpp
@@ -10,29 +10,40 @@ void Inky::updateGhost()
 {
 	if (isOnPacMan())
 		return;
-	//finish his move before recalculating
-	int dir = player->getDirection();
-	int row = player->Row();
-	int col = player->Col();
-
-	switch (dir)
+	if ((fmod(pixelX(), 16.0) == 0) && (fmod(pixelY(), 16.0) == 0)) ///only change direction when completely on a cell
 	{
-	case 1:
-		col -= 2;
-		break;
-	case 2:
-		col += 2;
-		break;
-	case 3:
-		row -= 2;
-		break;
-	case 4:
-		row += 2;
-		break;
-	default:
-		// just follow pacman's current tile
+		updateRowsorCols();
+		moving = false;
 	}
-	
-	int blinkyrow = blinky->getRow();
-	//backdrop of just target pacman?
+	if (moving) {
+		moveGhost();
+		renderGhost();
+	}
+	if (nodes.empty()) { //go to endpoint before running A* again
+		int dir = player->getDirection();
+		int row = player->Row();
+		int col = player->Col();
+
+		switch (dir)
+		{
+		case 1:
+			col -= 2;
+			break;
+		case 2:
+			col += 2;
+			break;
+		case 3:
+			row -= 2;
+			break;
+		case 4:
+			row += 2;
+			break;
+		default:   // just follow pacman's current tile
+		}
+
+		int rowvector = row - blinky->getRow();  //vectors from blinky's position to "ahead" tile, and then doubled
+		int colvector = blinky->getCol();
+
+		
+	}
 }

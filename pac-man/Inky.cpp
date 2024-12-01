6d33f898658e16fc8b9ca43acdeb65bae74b30a4
vishilp <vishilthe3rd@gmail.com>
2024-11-30 23:30:47 -0800

    working on inky movement

--- pac-man/Inky.cpp
+++ pac-man/Inky.cpp
@@ -10,15 +10,7 @@ void Inky::updateGhost()
 {
 	if (isOnPacMan())
 		return;
-	if ((fmod(pixelX(), 16.0) == 0) && (fmod(pixelY(), 16.0) == 0)) ///only change direction when completely on a cell
-	{
-		updateRowsorCols();
-		moving = false;
-	}
-	if (moving) {
-		moveGhost();
-		renderGhost();
-	}
+
 	if (nodes.empty()) { //go to endpoint before running A* again
 		int dir = player->getDirection();
 		int row = player->Row();
@@ -41,9 +33,40 @@ void Inky::updateGhost()
 		default:   // just follow pacman's current tile
 		}
 
-		int rowvector = row - blinky->getRow();  //vectors from blinky's position to "ahead" tile, and then doubled
-		int colvector = blinky->getCol();
+		int rowvector = 2 * (blinky->getRow() - row);  //vectors from blinky's position to "ahead" tile, and then doubled
+		int colvector = 2 * (blinky->getCol() - col);
+		int targetrow = blinky->getRow() - rowvector;
+		int targetcol = blinky->getCol() - colvector;
 
-		
+		moving = true;
+		NodeManager manager;
+		Node Inky(getRow(), getCol());
+		Node Target(targetrow, targetcol);
+		if (chaseMode() && !isOnPacMan())
+		{
+			nodes = findPath(map, Inky, Target, &manager);
+			if (!nodes.empty())
+				translateNodeToDir(nodes[0]);
+			moveGhost();
+			renderGhost();
+		}
+		return;
+
 	}
+
+	if ((fmod(pixelX(), 16.0) == 0) && (fmod(pixelY(), 16.0) == 0)) 
+	{
+		updateRowsorCols();
+		nodes.erase(nodes.begin());
+		if (!nodes.empty())
+			translateNodeToDir(nodes[0]);
+		else
+			moving = false;
+	}
+	if (moving) {
+		moveGhost();
+		renderGhost();
+		return;
+	}
+
 }
